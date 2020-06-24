2a40ba622cb5a49d17d18dc1cdb6d1c23a935a19
hamza herbou <hamza95herbou@gmail.com>
2020-06-24 19:40:30 +0100

    Upload files

---
+++ Assets/Test.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+	//Your Sprite
+	[SerializeField] SpriteRenderer imageSprite;
+	//drawing color
+	[SerializeField] Color drawColor;
+
+	//A copy of your sprite's texture
+	Texture2D imageTextureCopy;
+
+	void Start ()
+	{
+		//Check if already saved a texture in disk with key "myTexture"
+		if (TextureUtilities.IsTextureSaved ("myTexture")) {
+			//Load saved texture
+			imageTextureCopy = TextureUtilities.Load ("myTexture");
+
+			//Assign loaded texture to your sprite
+			imageSprite.sprite = TextureUtilities.GetSpriteFromTexture (imageTextureCopy);
+		}
+
+		//Get texture copy in order to manipulate its data and pixels.
+		//you can't access texture data directly, it will throw an error : (Texture is not readable)
+		imageTextureCopy = TextureUtilities.GetTextureCopy (imageSprite.sprite.texture);
+	}
+
+	void Update ()
+	{
+		if (Input.GetMouseButtonUp (0)) {
+			//If mouse click : Draw a random horizontal line with color assigned in the inspector
+
+			//Draw code
+			int randomY = Random.Range (3, imageTextureCopy.height - 4);
+			int y = randomY - 3;
+			for (int i = 0; i < 6; i++) {
+				for (int j = 0; j < imageTextureCopy.width; j++) {
+					imageTextureCopy.SetPixel (j, y, drawColor);
+				}
+				y++;
+			}
+
+			//Apply texture data
+			imageTextureCopy.Apply ();
+
+			//Assign this imageTextureCopy to your sprite
+			imageSprite.sprite = TextureUtilities.GetSpriteFromTexture (imageTextureCopy);
+		}
+	}
+
+	void OnApplicationQuit ()
+	{
+		//Save texture whenever you quit the game.
+		//Or hit Stop in unity Editor
+
+		TextureUtilities.Save (imageTextureCopy, "myTexture");
+	}
+}
---
+++ Assets/TextureUtilities.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class TextureUtilities : MonoBehaviour
+{
+	static string savePath = Application.persistentDataPath + "/SavedTextures/";
+
+	public static void Save (Texture2D texture, string key)
+	{
+		if (!Directory.Exists (savePath))
+			Directory.CreateDirectory (savePath);
+		
+		byte[] bytes = texture.EncodeToPNG ();
+
+		FileStream file = File.Create (GetFilePath (key));
+		file.Write (bytes, 0, bytes.Length);
+
+		file.Close ();
+	}
+
+	public static Texture2D Load (string key)
+	{
+		byte[] fileData = File.ReadAllBytes (GetFilePath (key));
+	
+		Texture2D texture = new Texture2D (2, 2);
+
+		//..this will auto-resize the texture dimensions.
+		texture.LoadImage (fileData); 
+
+		return texture;
+	}
+
+	public static bool IsTextureSaved (string key)
+	{
+		return File.Exists (GetFilePath (key));
+	}
+
+	public static Sprite GetSpriteFromTexture (Texture2D texture)
+	{
+		return Sprite.Create (
+			texture, 
+			new Rect (0, 0, texture.width, texture.height), 
+			new Vector2 (0.5f, 0.5f)
+		);
+	}
+
+
+	public static Texture2D GetTextureCopy (Texture2D source)
+	{
+		//Create a RenderTexture
+		RenderTexture rt = RenderTexture.GetTemporary (
+			                   source.width,
+			                   source.height,
+			                   0,
+			                   RenderTextureFormat.Default,
+			                   RenderTextureReadWrite.Linear
+		                   );
+
+		//Copy source texture to the new render (RenderTexture) 
+		Graphics.Blit (source, rt);
+
+		//Store the active RenderTexture & activate new created one (rt)
+		RenderTexture previous = RenderTexture.active;
+		RenderTexture.active = rt;
+
+		//Create new Texture2D and fill its pixels from rt and apply changes.
+		Texture2D readableTexture = new Texture2D (source.width, source.height);
+		readableTexture.ReadPixels (new Rect (0, 0, rt.width, rt.height), 0, 0);
+		readableTexture.Apply ();
+
+		//activate the (previous) RenderTexture and release texture created with (GetTemporary( ) ..)
+		RenderTexture.active = previous;
+		RenderTexture.ReleaseTemporary (rt);
+
+		return readableTexture;
+	}
+
+	static string GetFilePath (string filename)
+	{
+		return savePath + filename;
+	}
+}
